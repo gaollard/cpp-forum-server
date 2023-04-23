1550cd5f2c310cb7de13c187cf68f87c6697e45f
GaoXiong <xiong.gao@shopee.com>
2023-04-23 23:07:02 +0800

    chore: update

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <sys/queue.h>
 #include <event.h>
 #include <event2/http.h>
 #include <event2/http_struct.h>
 #include <event2/http_compat.h>
 #include <event2/util.h>
 #include <signal.h>
-#include "./include/db_query.h"
+#include "./include/_mysql.h"
+#include <vector>;
 
 #define LOG_INFO    printf
 #define LOG_DBG    printf
@@ -30,6 +30,11 @@ void show_help() {
     fprintf(stderr,"%s",help);
 }
 
+struct user {
+    int id;
+    char name[30];
+} stgirls;
+
 // 当向进程发出SIGTERM/SIGHUP/SIGINT/SIGQUIT的时候，终止event的事件侦听循环
 void signal_handler(int sig) {
     switch (sig) {
@@ -45,6 +50,57 @@ void signal_handler(int sig) {
 int main(int argc, char *argv[]) {
 
 //    db_query();
+    connection conn; // 数据库连接类
+
+    // 登录数据库，返回值：0-成功，其它-失败。
+    // 失败代码在conn.m_cda.rc中，失败描述在conn.m_cda.message中。
+    if (conn.connecttodb("127.0.0.1,root,123456,test,3306","utf-8") != 0)
+    {
+        printf("connect database failed.\n%s\n",conn.m_cda.message); return -1;
+    }
+
+    sqlstatement stmt(&conn); // 操作SQL语句的对象。
+
+    // 准备查询表的SQL语句。
+    stmt.prepare("select id,name from user where id>=:1");
+    int min_id = 15;
+    stmt.bindin(1,&min_id);
+
+    // 为SQL语句绑定输出变量的地址，bindout 方法不需要判断返回值。
+    stmt.bindout(1,&stgirls.id);
+    stmt.bindout(2, stgirls.name,30);
+
+    // 执行SQL语句，一定要判断返回值，0-成功，其它-失败。
+    // 失败代码在stmt.m_cda.rc中，失败描述在stmt.m_cda.message中。
+    if (stmt.execute() != 0)
+    {
+        printf("stmt.execute() failed.\n%s\n%s\n",stmt.m_sql,stmt.m_cda.message); return -1;
+    }
+    // db 查询测试
+    std::vector<user> users;
+
+    // 本程序执行的是查询语句，执行stmt.execute()后，将会在数据库的缓冲区中产生一个结果集。
+    while (1)
+    {
+        memset(&stgirls,0,sizeof(stgirls)); // 先把结构体变量初始化。
+
+        // 从结果集中获取一条记录，一定要判断返回值，0-成功，1403-无记录，其它-失败。
+        // 在实际开发中，除了0和1403，其它的情况极少出现。
+        if (stmt.next() !=0) break;
+
+        users.push_back(stgirls);
+
+        // 把获取到的记录的值打印出来。
+        printf("id=%ld,name=%s,weight=%.02f,btime=%s\n",stgirls.id,stgirls.name);
+    }
+
+    // 请注意，stmt.m_cda.rpc变量非常重要，它保存了SQL被执行后影响的记录数。
+    printf("本次查询了 girls 表%ld条记录。\n", stmt.m_cda.rpc);
+    printf("本次查询了 girls 表%ld条记录。\n", users.size());
+
+    for(std::vector<user>::iterator it = users.begin(); it != users.end();it++) {
+        printf("id=%ld, name=%s \n",it->id,it->name);
+    }
 
     //自定义信号处理函数
     signal(SIGHUP   , signal_handler);
