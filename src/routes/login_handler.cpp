3d5d1089c80c421d00350e76a8aa415876bb921d
GaoXiong <xiong.gao@shopee.com>
2023-05-21 00:03:15 +0800

    chore: 添加登录接口

--- src/routes/login_handler.cpp
+++ src/routes/login_handler.cpp
@@ -2,24 +2,35 @@
 // Created by Xiong Gao on 2023/4/19.
 //
 
-#include <stdio.h>
-#include <string.h>
-
+#include <iostream>
 #include <event.h>
 #include <event2/http.h>
 #include <event2/http_struct.h>
 #include "../../include/utils/send.h"
+#include "json/json.h"
 
 void login_handler(struct evhttp_request *req, void *arg) {
-    char output[2048] = "\0";
-    char tmp[1024];
-
     // 获取POST方法的数据
     char *post_data = (char *) EVBUFFER_DATA(req->input_buffer);
-    sprintf(tmp, "post_data=%s\n", post_data);
-    strcat(output, tmp);
 
-    // 输出的内容
-    char content[] = "login route";
-    send(req, content);
+    Json::Reader jsReader;
+    Json::Value jsonData;
+
+    if (!jsReader.parse(post_data , jsonData))
+    {
+        std::cout << "parse body error" << std::endl;
+        return;
+    }
+
+    Json::Value user_info;
+    user_info["id"] = 1;
+    user_info["account"] = jsonData["account"].asString();
+    user_info["password"] = jsonData["password"].asString();
+    user_info["token"] = "9527";
+
+    Json::StreamWriterBuilder builder;
+    builder["indentation"] = "";
+    std::string json_string = Json::writeString(builder, user_info);
+
+    send(req, json_string.data());
 }
